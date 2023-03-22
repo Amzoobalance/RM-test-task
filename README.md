7509c713c32664a7c426cc718bd8448536c6d9ff
Andrey Orlov <krotocat@gmail.com>
2023-03-22 14:34:10 +0700

    add: add readme description

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # RM-test-task
 
-### Before start :
+### 1.Clone this repository to your local machine
 
-- env
+### 2. Create .env file and assign values for the following variables
+
+- DATABASE_URL=your MySQL connection URL
+- SENDINBLUE_API_KEY=your Sendinblue API V3 Key
+- PORT=3000
+
+### 3. `npm i && npm run dev`
--- packages/api-service/index.js
+++ packages/api-service/index.js
@@ -6,7 +6,6 @@ const { getSchedule } = require("./endpoints/get-schedule");
 const { registerMySql } = require("./middleware/register-mysql");
 const { PrismaClient } = require("@prisma/client");
 const { emailServiceJob } = require("../email-service/cron");
-const { sendEmail } = require("../email-service/index");
 
 const client = new PrismaClient();
 const app = express();
@@ -20,8 +19,6 @@ app.post("/api/v1/appointments", appointmentToDoc);
 app.get("/api/v1/schedules/:date", getSchedule);
 
 emailServiceJob.start();
-
-// sendEmail("Андрей", "Терапевт", "930", "amzoo@vk.com", false);
 
 app.listen(process.env.PORT, () =>
   console.log(`API started on port ${process.env.PORT}`)
